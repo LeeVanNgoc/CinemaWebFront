beb27a94378d4382210a2021e45e8bbed75fb324
Vuyn <vuyndoes@gmail.com>
2024-07-12 15:41:31 +0700

    Add Carousel (Slide) for the Home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Footer from "./components/Common/Footer/Footer";
 import ResponsiveAppBar from "./components/Common/Header/Header";
+import Home from "./components/Screens/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <ResponsiveAppBar />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
